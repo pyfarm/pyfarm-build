963ddd8d23d51188c6efb2e8534e8d78843ad8b1
Oliver Palmer <oliverpalmer@opalmer.com>
2014-08-26 01:17:46 -0400

    initial connection of slaves, builder configs and triggers

--- master/buildmaster.py
+++ master/buildmaster.py
@@ -20,11 +20,12 @@ import yaml
 from buildbot.buildslave import BuildSlave
 from buildbot.buildslave.ec2 import EC2LatentBuildSlave
 from buildbot.config import BuilderConfig
+from buildbot.schedulers.triggerable import Triggerable
 from buildbot.status.web import authz, auth
 from buildbot.status import html
 
 from master.changesource import GitHubBuildBot
-from master.builders import get_build_factory
+from master.steps import get_build_factory
 
 
 def update(d, u):
@@ -47,121 +48,99 @@ with open("private.yml", "r") as private_yaml_file:
 # Merge the configurations
 file_cfg = update(public_config, private_config)
 
-buildslave_matrix = {
-    "linux": [],
-    "mac": [],
-    "windows": []
-}
-
-
 # Main configuration read in the .tac file
 config = BuildmasterConfig = {
     "title": "PyFarm",
     "titleURL": "https://build.pyfarm.net",
     "buildbotURL": "http://127.0.0.1:8010",
     "db_url": file_cfg["db"],
-    "status": [
-
-    ],
+    "status": [],
     "slaves": [],
+    "builders": [],
+    "schedulers": [],
     "protocols": {
         "pb": {"port": 9989}
     },
     "change_source": [
         GitHubBuildBot(file_cfg["github_changesource"]["secret"])
-    ],
-    "builders": [
-    ],
-
+    ]
 }
 
-# Persistent build slave(s)
+projects = ("core", "agent", "master")
+builders = config["builders"]
+schedulers = config["schedulers"]
+slaves = config["slaves"]
+slave_data = []
+
+# Add persistent build slave(s)
 for name, cfg in file_cfg["slaves"]["persistent"].items():
-    file_cfg["buildslave_matrix"][cfg.pop("type")] = name
-    config["slaves"].append(
-        BuildSlave(name, cfg.pop("passwd"), **cfg))
+    if not cfg.pop("enabled"):
+        continue
 
-# AWS build slave(s)
+    slave_data.append((name, cfg.pop("type"), map(str, cfg.pop("python"))))
+    slaves.append(BuildSlave(name, cfg.pop("passwd"), **cfg))
+
+# Add aws build slaves
 for name, cfg in file_cfg["slaves"]["aws"].items():
-    file_cfg["buildslave_matrix"][cfg.pop("type")] = name
-    config["slaves"].append(
-        EC2LatentBuildSlave(
-            name, cfg.pop("passwd"), cfg.pop("instance_type"), **cfg))
+    if not cfg.pop("enabled"):
+        continue
 
-# Construct the builder matrix
-for project, python_versions in file_cfg["python_versions"].items():
-    for python_version in python_versions:
-        for platform, buildslaves in buildslave_matrix.items():
+    slave_data.append((name, cfg.pop("type"), map(str, cfg.pop("python"))))
+    slaves.append(EC2LatentBuildSlave(
+        name, cfg.pop("passwd"), cfg.pop("instance_type"), **cfg))
+
+builder_names = []
+for project in projects:
+    project_builders = []
+    for slave, platform, python_versions in slave_data:
+        for python_version in python_versions:
+            name = "{project}-{platform}-{python_version}".format(**locals())
             builder = BuilderConfig(
-                name="{project}-{platform}-{python_version}".format(**locals()),
-                slavenames=buildslaves,
-                factory=get_build_factory(project, platform, python_version),
-                mergeRequests=True)
-            config["builders"].append(builder)
+                name=name, slavename=slave,
+                factory=get_build_factory(project, platform, python_version))
+            builders.append(builder)
+            builder_names.append(name)
+            project_builders.append(name)
+    scheduler = Triggerable(name=project, builderNames=project_builders)
+    schedulers.append(scheduler)
 
-
-
-
-####### SCHEDULERS
-
-# Configure the Schedulers, which decide how to react to incoming changes.  In this
-# case, just kick off a 'runtests' build
-
-from buildbot.schedulers.basic import SingleBranchScheduler
-from buildbot.schedulers.forcesched import ForceScheduler
-from buildbot.changes import filter
-config['schedulers'] = []
-config['schedulers'].append(SingleBranchScheduler(
-                            name="all",
-                            change_filter=filter.ChangeFilter(branch='master'),
-                            treeStableTimer=None,
-                            builderNames=["runtests"]))
-config['schedulers'].append(ForceScheduler(
-                            name="force",
-                            builderNames=["runtests"]))
-
-####### BUILDERS
-
-# The 'builders' list defines the Builders, which tell Buildbot how to perform a build:
-# what steps, and which slaves can execute them.  Note that any particular build will
-# only take place on one slave.
-
-from buildbot.process.factory import BuildFactory
-from buildbot.steps.source.git import Git
-from buildbot.steps.shell import ShellCommand
-
-factory = BuildFactory()
-# check out the source
-factory.addStep(Git(repourl='git://github.com/buildbot/pyflakes.git', mode='incremental'))
-# run the tests (note that this will require that 'trial' is installed)
-factory.addStep(ShellCommand(command=["trial", "pyflakes"]))
-
-
-
-config['builders'] = []
-config['builders'].append(
-    BuilderConfig(name="runtests",
-      slavenames=["example-slave"],
-      factory=factory))
-
-####### STATUS TARGETS
-
-# 'status' is a list of Status Targets. The results of each build will be
-# pushed to these targets. buildbot/status/*.py has a variety to choose from,
-# including web pages, email senders, and IRC bots.
-
-config['status'] = []
-
-authz_cfg=authz.Authz(
-    # change any of these to True to enable; see the manual for more
-    # options
-    auth=auth.BasicAuth([("pyflakes","pyflakes")]),
-    gracefulShutdown = False,
-    forceBuild = 'auth', # use this to test your slave once it is set up
-    forceAllBuilds = False,
-    pingBuilder = False,
-    stopBuild = False,
-    stopAllBuilds = False,
-    cancelPendingBuild = False,
-)
-config['status'].append(html.WebStatus(http_port=8010, authz=authz_cfg))
+#
+# ####### SCHEDULERS
+#
+# # Configure the Schedulers, which decide how to react to incoming changes.  In this
+# # case, just kick off a 'runtests' build
+#
+# from buildbot.schedulers.basic import SingleBranchScheduler
+# from buildbot.schedulers.forcesched import ForceScheduler
+# from buildbot.changes import filter
+# config['schedulers'] = []
+# config['schedulers'].append(SingleBranchScheduler(
+#                             name="all",
+#                             change_filter=filter.ChangeFilter(branch='master'),
+#                             treeStableTimer=None,
+#                             builderNames=["runtests"]))
+# config['schedulers'].append(ForceScheduler(
+#                             name="force",
+#                             builderNames=["runtests"]))
+#
+# ####### STATUS TARGETS
+#
+# # 'status' is a list of Status Targets. The results of each build will be
+# # pushed to these targets. buildbot/status/*.py has a variety to choose from,
+# # including web pages, email senders, and IRC bots.
+#
+# config['status'] = []
+#
+# authz_cfg=authz.Authz(
+#     # change any of these to True to enable; see the manual for more
+#     # options
+#     auth=auth.BasicAuth([("pyflakes","pyflakes")]),
+#     gracefulShutdown = False,
+#     forceBuild = 'auth', # use this to test your slave once it is set up
+#     forceAllBuilds = False,
+#     pingBuilder = False,
+#     stopBuild = False,
+#     stopAllBuilds = False,
+#     cancelPendingBuild = False,
+# )
+# config['status'].append(html.WebStatus(http_port=8010, authz=authz_cfg))
