758f3ea671ea4b0ecb6f12bf170b5793ef2d3907
Oliver Palmer <oliverpalmer@opalmer.com>
2014-08-30 11:56:15 -0400

    adding clone steps

--- master/master/steps.py
+++ master/master/steps.py
@@ -21,6 +21,7 @@ from buildbot.process.factory import BuildFactory
 from buildbot.steps.source.git import Git
 from buildbot.steps.shell import SetPropertyFromCommand
 from buildbot.steps.slave import RemoveDirectory
+from buildbot.steps.shell import ShellCommand
 from buildbot.process.properties import Property
 
 CREATE_ENVIRONMENT = """
@@ -29,26 +30,42 @@ import os
 import json
 import tempfile
 import subprocess
+
 tempdir = tempfile.mkdtemp()
-virtualenv = os.path.join(tempdir, "virtualenv")
-make_virtualenv = ["virtualenv", virtualenv, "--quiet"]
+virtualenv_root = os.path.join(tempdir, "virtualenv")
+short_pyversion = os.environ["PYTHON_VERSION"].replace(".", "")
 
 # Windows requires the site-packages directory so we can access things
 # like pywin32
 if os.name == "nt":
-    virtualenv.append("--system-site-packages")
+    mkvirtualenv = [
+        "C:\\Python%s\\Scripts\\virtualenv.exe" % short_pyversion,
+        virtualenv_root, "--system-site-packages", "--quiet"]
+    python_bin_name = "python.exe"
+    pip_bin_name = "pip.exe"
     bin_dir = "Scripts"
-    activate_script = "activate.bat"
+
 else:
+    mkvirtualenv = [
+        "virtualenv-%s" % os.environ["PYTHON_VERSION"], virtualenv_root,
+        "--quiet"]
+    python_bin_name = "python"
+    pip_bin_name = "pip"
     bin_dir = "bin"
-    activate_script = "activate"
 
 # Create the virtualenv
-subprocess.check_call(make_virtualenv)
+subprocess.check_call(mkvirtualenv)
 
-activate = os.path.join(virtualenv, bin_dir, activate_script)
+python = os.path.join(virtualenv_root, bin_dir, python_bin_name)
+pip = os.path.join(virtualenv_root, bin_dir, pip_bin_name)
+
 print(json.dumps(
-    {"virtualenv": virtualenv, "activate": activate, "tempdir": tempdir}))
+    {"virtualenv": virtualenv_root, "tempdir": tempdir,
+    "python": python, "pip": pip}))
+""".strip()
+
+CREATE_REQUIREMENTS = """
+
 """.strip()
 
 REPO_URL = "git://github.com/pyfarm/pyfarm-{project}.git"
@@ -93,6 +110,22 @@ def get_build_factory(project, platform, pyversion):
         CreateEnvironment(
             name="create environment",
             command=["python%s" % pyversion, "-c", CREATE_ENVIRONMENT]))
+
+    # Install 'core'
+    factory.addStep(
+        ShellCommand(
+            name="install pyfarm.core",
+            workdir="core",
+            command=[Property("pip"), "install", "-e", ".", "--egg"]))
+
+    # Install this package
+    if project != "core":
+        factory.addStep(
+            ShellCommand(
+                name="install pyfarm.%s" % project,
+                workdir=project,
+                command=[Property("pip"), "install", "-e", ".", "--egg"]))
+
 
     # Destroy the virtualenv
     factory.addStep(
